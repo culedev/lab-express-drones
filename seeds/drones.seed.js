678847a2f189a1b20b4b9d8134a5f5c76aa06f84
Ivan Culebra <ivan.culebra.munera@gmail.com>
2022-07-25 16:41:25 +0200

    Iteration 1

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,23 @@
 // Iteration #1
+require("../db");
+
+const { default: mongoose } = require("mongoose");
+const Drone = require("../models/Drone.model");
+
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
+
+const insertDrones = async () => {
+  try {
+    await Drone.insertMany(drones);
+    console.log("DB updated");
+    mongoose.connection.close();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+insertDrones();
